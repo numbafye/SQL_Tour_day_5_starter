68233d372ab8707b90408e24440c43e752bdd070
numbafye <saadiqgp@gmail.com>
2023-08-18 16:24:07 -0400

    sequelize downloaded and pg url added to env file.

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
-const { Sequelize } = require('sequelize')
-const bands = require('./controllers/bands_controller')
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
+const bands = require("./controllers/bands_controller");
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI);
+
+try {
+  sequelize.authenticate();
+  console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
+} catch (err) {
+  console.log(`Unable to connect to PG: ${err}`);
+}
 
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Tour API",
+  });
+});
 
-// CONTROLLERS  
-const bandsController = require('./controllers/bands_controller')
-app.use('/bands', bandsController)
+// CONTROLLERS
+const bandsController = require("./controllers/bands_controller");
+app.use("/bands", bandsController);
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
